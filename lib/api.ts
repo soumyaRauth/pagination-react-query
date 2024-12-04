d9ea37fc5a552a9d3a763be51b8968687f2e2df8
soumyaRauth <shoummorauth@live.co.uk>
2024-12-05 04:51:43 +0600

    error handling added to the api fetch function

--- lib/api.ts
+++ lib/api.ts
@@ -6,7 +6,14 @@ export const fetchPokemonList = async ({
   limit = 10,
   offset = 0,
 }: PaginationProp.PaginationPropsOptional): Promise<Pokemon.Multiple> => {
-  const response = await fetch(`${API_URL}?offset=${offset}&limit=${limit}`);
-  const data: Pokemon.Multiple = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${API_URL}?offset=${offset}&limit=${limit}`);
+    if (!response) {
+      throw new Error("Response unsuccessful!");
+    }
+    const data: Pokemon.Multiple = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("Could not fetch data");
+  }
 };
